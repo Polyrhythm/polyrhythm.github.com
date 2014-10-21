46b70738a146d4a24c883b00e9711c4e11eba267
Ryan <ryan.joshua.jones@gmail.com>
2014-10-21 14:39:25 -0700

    WOO

---
+++ blog/template/posts/2011-01-01-post1.html.clj
@@ -0,0 +1,14 @@
+; @layout  post
+; @title   Pagination
+; @tag     tag3
+
+(h1 (:page site))
+
+(p "Pagination setting.")
+##CLJ
+:index-template-regexp #"^index\.html\.clj$"
+:posts-per-page 2
+:page-filename-format "page$(page)/$(filename)"
+CLJ
+
+
